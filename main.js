cfb3e29b56e37f9dc51342729a10ba24efae36ef
Rokib <hasanrokibul413@gmail.com>
2025-12-01 21:50:07 +0600

    backend

---
+++ main.js
@@ -0,0 +1,42 @@
+const jsonServer = require("json-server");
+const customRouter = require("./router");
+const express = require("express");
+const checkAuth = require("./middleware/checkAuth");
+require("dotenv").config();
+const cors = require("cors");
+
+const app = jsonServer.create();
+const router = jsonServer.router("./database/db.json");
+
+app.use(cors({ credentials: true, origin: true }));
+app.use(express.static(__dirname + "/public"));
+app.use("/uploads", express.static("uploads"));
+
+// /!\ Bind the router db to the app
+app.db = router.db;
+
+app.use(express.json());
+
+// This middleware will add extra information before storing
+app.use(checkAuth);
+
+// CustomRoute Middleware to Handle Extra Routes
+app.use("/", customRouter);
+
+app.use(router);
+
+// Error handle Middleware
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, _next) => {
+  res.status(500).json({
+    error: err.message,
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Listening on port " + PORT);
+});
+
+module.exports = app;
